7c230603308b91e458ae882bccc33524db2b9b93
ChelisZC <jose-luis.zarate-castaneda@hpe.com>
2017-01-16 16:28:25 -0600

    Practice #1
    Exercise point #1

---
+++ hpe_training_js_lesson1_JOSE_LUIS.js
@@ -0,0 +1,11 @@
+//  -->  Practice #1 <--
+console.log("This");
+console.log("is");
+console.log("my");
+console.log("1st");
+console.log("JavaScript");
+console.log("Practice");
+console.log("and");
+console.log("need");
+console.log("to");
+console.log("print this 10 lines");
