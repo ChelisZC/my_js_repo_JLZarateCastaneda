23d7ef56a4b4099013c0782fdbdd20a7d46d83d9
ChelisZC <jose-luis.zarate-castaneda@hpe.com>
2017-01-17 12:43:34 -0600

    Commit1-Ene-17
    Ahi va jaja

--- hpe_training_js_lesson1_JOSE_LUIS.js
+++ hpe_training_js_lesson1_JOSE_LUIS.js
@@ -1,4 +1,6 @@
 //  -->  Practice #1 <--
+// P1 Excersice Point #1
+// Write at least 10 lines of js code that prints out a phrase, it can be whatever you wanna print
 console.log("This");
 console.log("is");
 console.log("my");
@@ -9,3 +11,33 @@ console.log("and");
 console.log("need");
 console.log("to");
 console.log("print this 10 lines");
+
+// P1 Excersice Point #2
+// Review the length of at least 5 words/sentences
+"my".length
+"1st".length
+"JavaScript".length
+"Practice".length
+"and".length
+
+//3.- Declare 5 different variables, it can be either a number or a String.
+var counter = 0;
+var String = "Type your text here...";
+var myAge = 30;
+var Excersice3 = "Declare 5 different variables...";
+var bool = "false";
+
+//4.- Ask a user 5 different questions.
+console.log("1.- What is your favorite food?");
+console.log("2.- Where would you like to staty on your last days?");
+console.log("3.- Who are the person that marks your LIFE/HEART?");
+console.log("4.- How can you help?");
+console.log("5.- When are you going to travel where you want to?");
+
+//5.- Declare at least 5 arrays, either only numbers, Strings or mixed.*/
+
+var myArray1 = ["3", "2", "1", "0"];
+var myName&Age = ["Jose Luis", "Zarate", "Castañeda", "30"];
+var Array2 = ["33", "3223", "321123", "32100123"];
+var myChildsNames = ["Yago", "Sinaí"];
+var myArrayPlacestoVisit = ["Mariposa Monarva", "Veracruz", "Huachimontones", "Secrets"];
