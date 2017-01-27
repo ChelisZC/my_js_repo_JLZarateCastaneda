bba589161784fdaca612018db258020e1e53a39f
ChelisZC <jose-luis.zarate-castaneda@hpe.com>
2017-01-27 17:10:07 -0600

    P3 Update @ 27 Ene

--- hpe_training_js_lesson3_JOSE_LUIS.js
+++ hpe_training_js_lesson3_JOSE_LUIS.js
@@ -11,31 +11,31 @@ Exercises for lesson 3:
 
 //1.- Using a For loop, print out numbers from 0 to 100 with a increase of 5 each.
 for (var i = 0; i <= 100; i = i + 5) {
-    console.log('P3 E1 @ Number: '+i);
+    console.log("P3 E1 @ Number: "+i);
 }
 //2.- Using a For loop, print all the values of an array by separate. Tip: You will need to use .length ;-)
 console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 var i2 = ["Men", "X", "Women", "Y"]
 for (var ii = 0; i2.length > ii; ii = ii +1) {
-    console.log('P3 E2 @ value: '+i2[ii]);
+    console.log("P3 E2 @ value: "+i2[ii]);
 }
 
 //- Using a For loop, print out numbers from 24 to 2 with a decrease of 2 each.
 console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 for (var i3 = 24; i3 >= 2; i3 = i3 - 2) {
-    console.log('P3 E3 @ Number: '+i3);
+    console.log("P3 E3 @ Number: "+i3);
 }
 
 //4.- IF used. .length comparation, and print numbers from 10 to 50 using for.
 console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 if (i2.length > 0){
   for (var i4 = 10; i4 <= 50; i4 = i4 + 1) {
-      console.log('P3 E4 @ Number: '+i4);
+      console.log("P3 E4 @ Number: "+i4);
      }
 }
 else {
   for (var i4 = 50; i4 >= 10; i4 = i4 - 1) {
-      console.log('P3 E4 @ Number: '+i4);
+      console.log("P3 E4 @ Number: "+i4);
     }
 }
 
@@ -43,11 +43,11 @@ else {
 console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 if (i2.length > 0){
   for (var i5 = 9; i5 >= 0; i5 = i5 - 1) {
-      console.log('P3 E5 @ Number: '+i5);
+      console.log("P3 E5 @ Number: "+i5);
      }
 }
 else {
   for (var i5 = 0; i5 <= 9; i5 = i5 + 1) {
-      console.log('P3 E5 @ Number: '+i5);
+      console.log("P3 E5 @ Number: "+i5);
      }
 }
